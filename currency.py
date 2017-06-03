800f5abe508f2ffa7a57ee11b88a0c8e56b2a317
MurdockHM <mmurdockh@gmail.com>
2017-06-03 22:10:51 +0200

    GG

--- currency.py
+++ currency.py
@@ -3,17 +3,10 @@ from bot import bot
 from GG import gg_manager as ggs
 
 
-@bot.command(pass_context=True)
-async def update_ggs(ctx):
-    """Super secret bot owner only command."""
-    if ctx.message.author.id != '178887072864665600':
-        await bot.say("You're not kewl enough.")
-        return
-    for server in bot.servers:
+@bot.event
+async def on_server_join(server):
         for member in server.members:
             ggs.add_user(member.id, member.name)
-
-    await bot.say("Successfully updated.")
 
 
 @bot.command(pass_context=True)
@@ -29,15 +22,15 @@ async def balance(ctx, member: discord.Member=None):
 
 
 @bot.command(pass_context=True)
-async def cheat(ctx, num: int, member: discord.Member):
+async def cheat(ctx, num: int, *members: discord.Member):
     """You saw nothing."""
     if ctx.message.author.id != '178887072864665600':
         await bot.say("You're not kewl enough.")
         return
 
-    if num >= 0:
-        ggs.add(member.id, num)
-    else:
-        ggs.sub(member.id, num)
-    current = ggs.get_ggs(member.id)
-    await bot.say("{}'s balance is now: {} GGs.".format(member.name, current))
+    for member in members:
+        if num >= 0:
+            ggs.add(member.id, num)
+        else:
+            ggs.sub(member.id, num)
+    await bot.say("Added {} GGs.".format(num))
