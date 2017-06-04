405a05bfa144aa3ab35e48290719172cc98d5033
MurdockHM <mmurdockh@gmail.com>
2017-06-04 12:53:14 +0200

    Yay

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 from discord.ext import commands
 from GG import gg_manager as ggs
 import random
+import aiohttp
 
 
 description = """It's gonna be kewl soon"""
@@ -22,6 +23,17 @@ async def on_message(message):
     if random.randint(0, 99) < 5:
         author = message.author
         ggs.add(author.id, 10)
+
+
+@bot.command(pass_context=True)
+async def change_avatar(ctx, url: str):
+    if ctx.message.author.id != '178887072864665600':
+        await bot.say("You're not Zack enough.")
+        return
+    with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            avatar_b = await resp.read()
+            bot.edit_profile(avatar=avatar_b)
 
 
 @bot.command(pass_context=True)
