7a14dbac2b525ac5174ffd715623fb87e22a112c
MurdockHM <mmurdockh@gmail.com>
2017-06-07 13:55:39 +0200

    GG

--- GG.py
+++ GG.py
@@ -1,42 +1,75 @@
-import json
+import os
+import psycopg2
+import urllib.parse as urlparse
+
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["HEROKU_POSTGRESQL_SILVER_URL"])
 
 
 class GGManager:
 
     def __init__(self):
-        self.db_file = 'db/ggs.json'
-        with open(self.db_file, mode='r', encoding='utf-8') as f:
-            self.ggs_data = json.load(f)
+        global url
+        self.ggs_data = {}
+        self.conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+        self.cur = self.conn.cursor()
+        self.cur.execute("SELECT * FROM users;")
+        query = self.cur.fetchall()
+        for row in query:
+            _id, ggs, username = row
+            self.ggs_data[int(_id)] = {'name': str(username), 'ggs': int(ggs)}
 
     def save(self):
-        with open(self.db_file, mode='w', encoding='utf-8') as f:
-            json.dump(self.ggs_data, f)
+        self.conn.commit()
 
     def add_user(self, _id, name):
-        if str(_id) in self.ggs_data:
+        _id = int(_id)
+        if _id in self.ggs_data:
             return
-        self.ggs_data[str(_id)] = {'name': name, 'ggs': 10}
+        self.ggs_data[_id] = {'name': name, 'ggs': 10}
+        self.cur.execute("INSERT INTO users (id, ggs, username) VALUES (%s, %s, %s)", (_id, 10, name))
         self.save()
 
     def get_ggs(self, _id):
-        return self.ggs_data[str(_id)]['ggs']
+        _id = int(_id)
+        return self.ggs_data[_id]['ggs']
 
     def add(self, _id, n):
+        _id = int(_id)
         n = abs(n)
-        self.ggs_data[str(_id)]['ggs'] += n
+        self.ggs_data[_id]['ggs'] += n
+        ggs = self.get_ggs(_id)
+        self.cur.execute("UPDATE users SET ggs=%s WHERE id=%s", (ggs, _id))
         self.save()
 
     def sub(self, _id, n):
+        _id = int(_id)
         n = abs(n)
-        self.ggs_data[str(_id)]['ggs'] -= n
+        self.ggs_data[_id]['ggs'] -= n
+        ggs = self.get_ggs(_id)
+        self.cur.execute("UPDATE users SET ggs=%s WHERE id=%s", (ggs, _id))
         self.save()
 
     def set(self, _id, n):
-        self.ggs_data[str(_id)]['ggs'] = n
+        _id = int(_id)
+        self.ggs_data[_id]['ggs'] = n
+        self.cur.execute("UPDATE users SET ggs=%s WHERE id=%s", (n, _id))
         self.save()
 
     def entry_exists(self, _id):
+        _id = int(_id)
         return self.ggs_data.get(_id, default=False)
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
 
 
 gg_manager = GGManager()
