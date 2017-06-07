b3078c9253ba30b1c439160c51a5d5ab5f2bd833
MurdockHM <mmurdockh@gmail.com>
2017-06-07 18:48:24 +0200

    Yay

---
+++ image_fun.py
@@ -0,0 +1,32 @@
+import discord
+from bot import bot
+import aiohttp
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+
+
+@bot.command(pass_context=True)
+async def uwot(ctx, member: discord.Member, *args):
+    url = member.avatar_url
+    with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            avatar = await resp.read()
+    base = Image.open('images/uwot.png').convert('RGBA')
+    av = Image.open(avatar).convert('RGBA')
+    av = ImageOps.fit(av, (260, 260))
+
+    txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
+
+    fnt = ImageFont.truetype('fonts/big_noodle_titling.ttf', 17)
+    d = ImageDraw.Draw(txt)
+
+    res = ""
+    for a in args:
+        res += a
+
+    d.text((455, 450), res, font=fnt, fill=(0, 0, 0, 255))
+
+    txt = txt.rotate(-25, resample=Image.BILINEAR)
+    out = Image.alpha_composite(base, txt)
+    out.paste(av, box=(85, 360))
+
+    bot.send_file(out, ctx.message.channel)
--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
 from bot import bot
 import currency
 import games
+import image_fun
 
 bot.run('MzE4MzkwNDcwNTI1MDU5MDcy.DAxtVQ.vD94PBVL5OYKV_6KbWA4rkITUWc')
