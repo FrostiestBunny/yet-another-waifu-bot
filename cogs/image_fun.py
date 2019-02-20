31862b4a7fa62ebc03a7280c40c458d328d1923c
ZackUnfair <acezackfair@gmail.com>
2019-02-20 17:49:38 +0100

    Improve image editing

--- cogs/image_fun.py
+++ cogs/image_fun.py
@@ -12,13 +12,22 @@ class ImageFun:
 
     @command(pass_context=True)
     async def uwot(self, ctx: Context, member: discord.Member, *args):
+        author = ctx.message.author
         url = member.avatar_url
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
-                avatar = await resp.read()
+                target_avatar = await resp.read()
+        url = author.avatar_url
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                author_avatar = await resp.read()
         base = Image.open('images/uwot.png').convert('RGBA')
-        av = Image.open(BytesIO(avatar)).convert('RGBA')
-        av = ImageOps.fit(av, (260, 260))
+        target_av = Image.open(BytesIO(target_avatar)).convert('RGBA')
+        target_av = ImageOps.fit(target_av, (260, 260))
+        target_av2 = Image.open(BytesIO(target_avatar)).convert('RGBA')
+        target_av2 = ImageOps.fit(target_av2, (114, 114))
+        author_av = Image.open(BytesIO(author_avatar)).convert('RGBA')
+        author_av = ImageOps.fit(author_av, (122, 122))
 
         txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
 
@@ -34,7 +43,9 @@ class ImageFun:
 
         txt = txt.rotate(-25, resample=Image.BILINEAR)
         out = Image.alpha_composite(base, txt)
-        out.paste(av, box=(85, 360))
+        out.paste(target_av, box=(85, 360))
+        out.paste(author_av, box=(116, 33))
+        out.paste(target_av2, box=(477, 8))
         output = BytesIO()
         out.save(output, format="PNG")
         output.seek(0)
