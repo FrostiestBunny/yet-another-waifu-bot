f1776c415a88e1c49977a848be03dafe4cb2c6cf
ZackUnfair <acezackfair@gmail.com>
2019-03-25 16:26:01 +0100

    Update bot_config and remove unnecessary waifu_commands.py So that bot config can now use any property

--- bot_config.py
+++ bot_config.py
@@ -3,7 +3,8 @@ class BotConfig:
     def __init__(self):
         self.conn = None
         self.cur = None
-        self.spawn_channel_id = None
+        self.config = {}
+        self.config_props = ['spawn_channel_id', 'spawn_rate']
 
     def connect(self, conn):
         self.conn = conn
@@ -11,27 +12,28 @@ class BotConfig:
         self.load_config()
     
     def load_config(self):
-        self.cur.execute("SELECT spawn_channel FROM bot_config;")
+        columns = ', '.join(self.config_props)
+        self.cur.execute("SELECT %s FROM bot_config;", (columns,))
         query = self.cur.fetchone()
         if query is None:
             return
-        spawn_channel_id = query[0]
-        self.spawn_channel_id = spawn_channel_id
+        for i, c in enumerate(query):
+            self.config[self.config_props[i]] = c
     
-    def update_config(self, spawn_channel_id):
-        if self.spawn_channel_id is not None:
-            self.alter_config(spawn_channel_id)
+    def update_config(self, prop, value):
+        if self.config.get(prop, None) is not None:
+            self.alter_config(prop, value)
             return
-        self.spawn_channel_id = spawn_channel_id
-        self.cur.execute("INSERT INTO bot_config (spawn_channel) VALUES (%s)",
-                         (self.spawn_channel_id,))
+        self.config[prop] = value
+        self.cur.execute("INSERT INTO bot_config (%s) VALUES (%s)",
+                         (prop, value))
         self.save()
 
-    def alter_config(self, spawn_channel_id):
-        self.cur.execute("UPDATE bot_config SET spawn_channel = %s WHERE spawn_channel=%s",
-                         (spawn_channel_id, self.spawn_channel_id,))
+    def alter_config(self, prop, value):
+        self.cur.execute("UPDATE bot_config SET %s = %s WHERE %s=%s",
+                         (prop, value, prop, self.config[prop]))
         self.save()
-        self.spawn_channel_id = spawn_channel_id
+        self.config[prop] = value
 
     def save(self):
         self.conn.commit()
